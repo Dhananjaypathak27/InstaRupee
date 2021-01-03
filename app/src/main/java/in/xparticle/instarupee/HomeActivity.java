c48ae8acd4737cf8e3e91632c31948851b075634
Dhananjaypathak27 <43525854+Dhananjaypathak27@users.noreply.github.com>
2021-01-03 11:36:42 +0530

    bottom_nav_menu.xml added in HomeActivity

--- app/src/main/java/in/xparticle/instarupee/HomeActivity.java
+++ app/src/main/java/in/xparticle/instarupee/HomeActivity.java
@@ -4,11 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import in.xparticle.instarupee.fragment.HomeFragment;
+
 public class HomeActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setBackground(null);
+        bottomNavigationView.getMenu().getItem(2).setEnabled(false);
+
+        intent_function();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
+
+            
+
+    }
+
+    private void intent_function() {
+
+
+
     }
 }
--- app/src/main/java/in/xparticle/instarupee/LoginActivity.java
+++ app/src/main/java/in/xparticle/instarupee/LoginActivity.java
@@ -2,13 +2,26 @@ package in.xparticle.instarupee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button mLoginBtn,mSignUpBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mLoginBtn = findViewById(R.id.activity_login_loginBtn);
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
