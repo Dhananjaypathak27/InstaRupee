f201aff370bf95a5417d989e7cc1978ad88bda25
Dhananjaypathak27 <43525854+Dhananjaypathak27@users.noreply.github.com>
2021-01-04 11:35:33 +0530

    SignUpActivity - spinner added to state and city (user can select state and city form predefined data)

--- app/src/main/java/in/xparticle/instarupee/SignUpActivity.java
+++ app/src/main/java/in/xparticle/instarupee/SignUpActivity.java
@@ -7,8 +7,11 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -21,10 +24,14 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    Spinner spinnerState,spinnerCity;
+    String state ="",city="";
     private Button mLoginBtn,mSignUpBtn;
-    private EditText firstName,lastName,mobileNumber,email,city,state,password;
+    private EditText firstName,lastName,mobileNumber,email,
+            //city, state,
+            password;
     private ProgressDialog loadingBar;
 
     @Override
@@ -36,8 +43,20 @@ public class SignUpActivity extends AppCompatActivity {
         lastName = findViewById(R.id.activity_signup_lastname);
         mobileNumber = findViewById(R.id.activity_signup_phonenumber);
         email = findViewById(R.id.activity_signup_email);
-        city = findViewById(R.id.activity_signup_city);
-        state = findViewById(R.id.activity_signup_state);
+        spinnerCity = findViewById(R.id.activity_signup_city);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,R.array.india_top_places,R.layout.support_simple_spinner_dropdown_item);
+        adapter1.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinnerCity.setAdapter(adapter1);
+        spinnerCity.setOnItemSelectedListener(this);
+        //state = findViewById(R.id.activity_signup_state);
+
+
+        spinnerState = findViewById(R.id.activity_signup_state);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.india_states,R.layout.support_simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinnerState.setAdapter(adapter);
+        spinnerState.setOnItemSelectedListener(this);
+
         password = findViewById(R.id.activity_signup_password);
         mLoginBtn = findViewById(R.id.activity_signup_loginBtn);
         mSignUpBtn = findViewById(R.id.activity_signup_signupbtn);
@@ -80,11 +99,13 @@ public class SignUpActivity extends AppCompatActivity {
         else if (email.getText().toString().equals("")){
             email.setError("Enter Email");
         }
-        else if (city.getText().toString().equals("")){
-            city.setError("Enter Your City Name");
+        else if (city.equals("Select your City")){
+            //city.setError("Enter Your City Name");
+            Toast.makeText(this, "No City Selected", Toast.LENGTH_SHORT).show();
         }
-        else if (state.getText().toString().equals("")){
-            state.setError("Enter state");
+        else if (state.equals("Select your State")){
+            //state.setError("Enter state");
+            Toast.makeText(this, "No state Selected", Toast.LENGTH_SHORT).show();
         }
         else if (password.getText().toString().length()<10){
             password.setError("Password must be at least 10 digit");
@@ -96,7 +117,7 @@ public class SignUpActivity extends AppCompatActivity {
             loadingBar.show();
 
             userValidation(firstName.getText().toString(),lastName.getText().toString(),mobileNumber.getText().toString(),email.getText().toString(),
-                    city.getText().toString(),state.getText().toString(),password.getText().toString());
+                    city,state,password.getText().toString());
 
         }
 
@@ -155,4 +176,17 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        if(adapterView.getId() == R.id.activity_signup_state)
+        state = adapterView.getItemAtPosition(i).toString();
+
+        if(adapterView.getId() == R.id.activity_signup_city)
+        city = adapterView.getItemAtPosition(i).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
 }
