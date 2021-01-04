800acc01ad8b8e6211e98b2271ccb15d33a54915
Dhananjaypathak27 <43525854+Dhananjaypathak27@users.noreply.github.com>
2021-01-04 12:58:10 +0530

    ProfileFragment - logout option added (using alert dialog) ManageProfileActivity added

---
+++ app/src/main/java/in/xparticle/instarupee/ManageProfileActivity.java
@@ -0,0 +1,14 @@
+package in.xparticle.instarupee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManageProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_profile);
+    }
+}
--- app/src/main/java/in/xparticle/instarupee/fragment/ProfileFragment.java
+++ app/src/main/java/in/xparticle/instarupee/fragment/ProfileFragment.java
@@ -1,66 +1,95 @@
 package in.xparticle.instarupee.fragment;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import in.xparticle.instarupee.MainActivity;
+import in.xparticle.instarupee.ManageProfileActivity;
 import in.xparticle.instarupee.R;
+import in.xparticle.instarupee.utils.AppSession;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ProfileFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    LinearLayout manageProfile,help,aboutUs,logout;
+    AppSession appSession;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        manageProfile = view.findViewById(R.id.manageProfile);
+        help = view.findViewById(R.id.help);
+        appSession =new  AppSession(getActivity());
+        aboutUs = view.findViewById(R.id.aboutUs);
+        logout = view.findViewById(R.id.logout);
+
+        manageProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                manageProfileFun();
+            }
+        });
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logoutFromThisDevice();
+            }
+        });
+    }
+
+    private void logoutFromThisDevice() {
+
+        AlertDialog alertDialog = new AlertDialog.Builder(getContext())
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Are you sure to Logout")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        appSession.setLogin(false);
+                        appSession.setFirstName(null);
+                        appSession.setFirstName(null);
+                        appSession.setState(null);
+                        appSession.setCity(null);
+                        appSession.setPhoneNumber(null);
+                        appSession.setPassword(null);
+                        appSession.setEmail(null);
+                        Intent intent = new Intent(getActivity(), MainActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .show();
+
+    }
+
+    private void manageProfileFun() {
+        Intent intent = new Intent(getActivity(), ManageProfileActivity.class);
+        startActivity(intent);
     }
 }
